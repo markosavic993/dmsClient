b24ae7a75fbae409454161898f24c7e989c4004b
Marko Savic <Marko.Savic@zuhlke.com>
2017-02-08 23:25:57 +0100

    add angular routing

--- app/app.js
+++ app/app.js
@@ -1,8 +1,21 @@
 /**
  * Created by msav on 2/4/2017.
  */
+"use strict";
 
-angular.module("dmsApp", []);
+var app = angular.module("dmsApp", ["ngRoute"]);
 
-
-
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "partials/registration_form.html",
+            controller: "registrationFormController"
+        })
+        .when("/dashboard", {
+            templateUrl: "partials/dashboard.html"
+        })
+        .otherwise({
+                templateUrl: "partials/notFound.html"
+            }
+        )
+});
