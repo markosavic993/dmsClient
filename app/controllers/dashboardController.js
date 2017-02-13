f45333cc4f00a93750525b8c414751a2d3dacc77
Pejovic <nenad.pejovic174@gmail.com>
2017-02-13 08:24:38 -0800

    dinamically set content of dashboard div

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -3,8 +3,23 @@
  */
 var app = angular.module("dmsApp");
 
-app.controller("dashboardController", function ($location, cssInjector) {
+app.controller("dashboardController", function ($scope, $location, cssInjector) {
     var vm = this;
 
     cssInjector.add("../css/dashboard.css");
+
+    $scope.templates = [
+        { name: 'activites', url: '../partials/activities.html'},
+        { name: 'processes', url: '../partials/processes.html'}
+    ];
+
+    $scope.template = $scope.templates[0];
+
+    $scope.showProcessesTab = function(){
+        $scope.template = $scope.templates[1];
+    }
+
+    $scope.showActivitiesTab = function () {
+        $scope.template = $scope.templates[0];
+    }
 });
--- app/services/registrationFormService.js
+++ app/services/registrationFormService.js
@@ -6,13 +6,13 @@ var app = angular.module("dmsApp");
 app.factory("registrationService", function ($http) {
     return {
         login: function (loginInfo) {
-            return $http.post("http://localhost:8080/user/login", loginInfo);
+            return $http.post("http://localhost:8080/dms/user/login", loginInfo);
         },
         signup: function (signupInfo) {
-            return $http.post("http://localhost:8080/user/signup", signupInfo);
+            return $http.post("http://localhost:8080/dms/user/signup", signupInfo);
         },
         registerCompany: function (companyInfo) {
-            return $http.post("http://localhost:8080/company/register", companyInfo);
+            return $http.post("http://localhost:8080/dms/company/register", companyInfo);
         }
     }
 });
