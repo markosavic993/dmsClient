1f11898b4d490eb48f4383e1a44824c457f3ad25
Marko Savic <Marko.Savic@zuhlke.com>
2017-02-26 21:44:57 +0100

    add functionality for uploading files (needs a lot of refactoring)

---
+++ app/directives/fileModel.js
@@ -0,0 +1,19 @@
+/**
+ * Created by msav on 2/26/2017.
+ */
+var app = angular.module("dmsApp");
+app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
