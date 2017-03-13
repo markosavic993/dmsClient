b05e264c4aba55d4d7ba5e3b397fb2bf4ec98b92
Nenad <nenad.pejovic174@gmail.com>
2017-03-13 08:43:29 +0100

    unstaged files

---
+++ app/services/processService.js
@@ -0,0 +1,20 @@
+/**
+ * Created by PredragiNenad on 3/12/2017.
+ */
+var app = angular.module("dmsApp");
+
+app.factory("processService", function ($http, configService) {
+    return {
+        addComplexProcess: function(process){
+            process.company = configService.getConfig().company;
+            return $http.post("http://localhost:8080/dms/process/complex/add",process);
+        },
+        addPrimitiveProcess: function(process){
+            process.company = configService.getConfig().company;
+            return $http.post("http://localhost:8080/dms/process/primitive/add",process);
+        },
+        deleteProcess: function(process){
+            return $http.post("http://localhost:8080/dms/process/delete/",process);
+        }
+    }
+});
