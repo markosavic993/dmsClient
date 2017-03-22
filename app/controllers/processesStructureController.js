2e15572387425a2049456e0b3e165df2937659b1
Nenad <nenad.pejovic174@gmail.com>
2017-03-22 22:12:04 +0100

    actions for adding and deleting on every node

--- app/controllers/processesStructureController.js
+++ app/controllers/processesStructureController.js
@@ -9,10 +9,7 @@ app.controller('processesStructureController', function ($scope, configService, 
     $scope.process = {parentProcess: {processName: ""}}
     $scope.modalTemplates = [
         {name: 'add_activity', url: '../partials/modal_add_activity.html'},
-        {name: 'add_process', url: '../partials/modal_add_process.html'},
-        {name: 'delete_process', url: '../partials/modal_delete_process.html'},
-        {name: 'delete_process', url: '../partials/modal_delete_activity.html'}
-
+        {name: 'add_process', url: '../partials/modal_add_process.html'}
     ];
 
     $scope.types = [
@@ -22,34 +19,24 @@ app.controller('processesStructureController', function ($scope, configService, 
 
     $scope.modalTemplate = $scope.modalTemplates[0];
 
-    $scope.openAddActivityModal = function () {
+    $scope.openAddActivityModal = function (process) {
         dashboardService.loadDocuments().then(function(response){
             $scope.documents = response.data;
             $scope.activity.inputDocument = $scope.documents[0];
             $scope.activity.outputDocument = $scope.documents[0];
         });
+        $scope.process = process;
         $scope.modalTemplate = $scope.modalTemplates[0];
     }
 
-    $scope.openAddProcessModal = function () {
+    $scope.openAddProcessModal = function (process) {
+        $scope.parentProcess = process;
         $scope.modalTemplate = $scope.modalTemplates[1];
     }
-
-    $scope.openDeleteProcessModal = function () {
-        $scope.modalTemplate = $scope.modalTemplates[2];
-    }
-
-    $scope.openDeleteActivityModal = function () {
-        configService.retrieveActivities();
-        $scope.activities = configService.getConfig().activities;
-        $scope.modalTemplate = $scope.modalTemplates[3];
-    }
-
     $scope.addProcess = function (process, processType) {
 
-        if(process.parentProcess.processName == ""){
-            process.parentProcess = null;
-        }
+        process.parentProcess = $scope.parentProcess;
+
         if (processType == "primitive") {
             process.type = "PrimitiveProcess";
             processService.addPrimitiveProcess(process).then(function () {
@@ -64,6 +51,7 @@ app.controller('processesStructureController', function ($scope, configService, 
     }
 
     $scope.addActivity = function(activity){
+        activity.process = $scope.process;
         activityService.addActivity(activity).then(function(){
             reloadProcesses();
         })
